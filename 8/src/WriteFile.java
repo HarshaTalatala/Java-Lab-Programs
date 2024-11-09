a7c55f12d53c5d308bb72671084006d14ae7cce1
Harsha Vardhan Reddy Talatala <harsha.talatala@gmail.com>
2024-11-09 12:26:55 +0530

    Lab Programs

--- 8/src/WriteFile.java
+++ 8/src/WriteFile.java
@@ -1,10 +1,10 @@
 import java.io.*;
 class WriteFile{
     public static void main(String[] args) throws Exception{
-        FileOutputStream fout = new FileOutputStream("hello.txt");
+        FileWriter fout = new FileWriter("hello.txt");
         String s = "Vishnu Institute of Technology...";
-        byte[] b= s.getBytes();
-        fout.write(b);
+        fout.write(s);
         fout.close();
+        System.out.println("written successfully");
     }
 }
